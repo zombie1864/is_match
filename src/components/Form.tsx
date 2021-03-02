662ddd867a20bbffd21695ee4083f970b1dcbce7
zombie1864 <zombie1864@gmail.com>
2021-03-02 15:59:01 -0500

    added basic structure of the form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -23,32 +23,42 @@ class Form extends Component<{}, Istate> {
       },
       userInteraction: false, 
 			btnDisabled: false, 
-      mount: true 
+      mount: false 
     }
   }
-  mountCounter = () => this.setState({mount:true})
-  unmountCounter = () => this.setState({mount:false})
-  private formFunc = ():any => {
+
+  private mountComp = () => this.setState({mount:true})
+  private unmountComp = () => this.setState({mount:false})
+
+  private onChange = (event:any):void => {
+    this.unmountComp()
+    const newState = { formFields: { ...this.state.formFields, [event.target.name]: (event.target.value) } } as Istate; // look into why the spreed op works 
+    this.setState( newState )
+  }
+  private formFunc = ():any => { // the value={ ... } not modular enough do research into this 
     let formContainer
     let formFields = Object.keys(this.state.formFields) // arrOfKeys 
     formContainer = 
       <div>
         <form>
-          {formFields.map( formField => {
-            return <label key={formField}>{formField[0].toUpperCase() + formField.slice(1)}<input type="text"/></label>
+          {formFields.map( (formField, idx) => {
+            return <label key={idx}>
+                {formField[0].toUpperCase() + formField.slice(1)}: 
+                <input type="text" placeholder='0' onChange={this.onChange} name={formField} value={ idx === 0 ? this.state.formFields.minimum : idx === 1 ? this.state.formFields.maximum : this.state.formFields.target }/>
+              </label>
           })}
         </form>
       </div>
     return formContainer 
   }
-  public render():JSX.Element {
 
+  public render():JSX.Element {
+    console.log(this.state);
     return (
       <div>
-        <button onClick={this.mountCounter} disabled={this.state.mount}>Mount</button>
-        <button onClick={this.unmountCounter} disabled={!this.state.mount}>unmount</button>
         <h1>form</h1>
         {this.formFunc()}
+        <button onClick={this.mountComp} disabled={this.state.mount}>Run Target</button>
         {this.state.mount ? <Table />: null }
         {this.state.mount ? <PieChart />: null }
       </div>
