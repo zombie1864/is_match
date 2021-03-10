8c29979f7d078c04b50275f0b82d1ebc1c211d15
zombie1864 <zombie1864@gmail.com>
2021-03-10 08:28:24 -0500

    added testing

---
+++ src/components/validators.test.ts
@@ -0,0 +1,32 @@
+import { isNumValidator, formFieldsValidator } from './validators'
+
+test('isNumValidator should return true for inputs of type number, false for other types', async () => {
+    expect(isNumValidator('8')).toBe(true)
+    expect(isNumValidator('0')).toBe(true)
+})
+
+test('isNumValidator should return false for negative numbers', async () => {
+    expect(isNumValidator('-8')).toBe(false)
+})
+
+test('isNumValidator should return false for decimal numbers', async () => {
+    expect(isNumValidator('8.1')).toBe(false)
+})
+
+test('isNumValidator should return false for inputs of other types', async () => {
+    expect(isNumValidator('8o')).toBe(false)
+    expect(isNumValidator('pop')).toBe(false)
+    expect(isNumValidator('z')).toBe(false)
+    expect(isNumValidator(' ')).toBe(false)
+})
+
+test('formFieldsValidator should take in an object of type string with attr minimum, maximum, and target. If any value is empty string should return false', async () => {
+    expect(formFieldsValidator({ minimum:'', maximum:'', target:''})).toBe(false)
+    expect(formFieldsValidator({ minimum:'8', maximum:'', target:''})).toBe(false)
+    expect(formFieldsValidator({ minimum:'', maximum:'8', target:''})).toBe(false)
+    expect(formFieldsValidator({ minimum:'', maximum:'', target:'8'})).toBe(false)
+    expect(formFieldsValidator({ minimum:'8', maximum:'8', target:''})).toBe(false)
+    expect(formFieldsValidator({ minimum:'', maximum:'8', target:'8'})).toBe(false)
+    expect(formFieldsValidator({ minimum:'8', maximum:'', target:'8'})).toBe(false)
+})
+
